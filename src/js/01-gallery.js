b834de7f9ab5d7c69da17474739f41c6a5f17c4a
Viktar Baloshka <vboloshko@gmail.com>
2023-04-03 14:58:23 +0200

    home

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
+
+const galleryContainer = document.querySelector('.gallery');
+galleryContainer.insertAdjacentHTML('beforeend', createGallery(galleryItems));
+
+function createGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+                <a class="gallery__link" href = "${original}">
+                    <img class=gallery__image
+                        src="${preview}"
+                        alt="${description}">
+                </a>
+            </li>`;
+    })
+    .join('');
+}
+new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
